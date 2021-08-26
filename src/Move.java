1d1d195a965f92080d3b4fe0ab42736c4cd61cd8
valerkaparkalov <valeraparkalov997@gmail.com>
2021-08-26 09:29:15 +0300

    task2

--- src/Move.java
+++ src/Move.java
@@ -8,11 +8,44 @@ public class Move {
     private char nextValue;
     private int levelLocate;
     private Map generateLocate;
+    private int counter;
     private boolean booleanWile;
+    private int partOneValue = 0;
     Random random = new Random();
     Heroes heroes = new Heroes();
     List numberStep = new ArrayList(2);
 
+    public int getPartOneValue() {
+        return partOneValue;
+    }
+
+    public void setPartOneValue(int partOneValue) {
+        this.partOneValue = partOneValue;
+    }
+
+    public char getNextValue() {
+        return nextValue;
+    }
+
+    public void setNextValue(char nextValue) {
+        this.nextValue = nextValue;
+    }
+
+    public int getLevelLocate() {
+        return levelLocate;
+    }
+
+    public void setLevelLocate(int levelLocate) {
+        this.levelLocate = levelLocate;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
 
     public Map getGenerateLocate() {
         return generateLocate;
@@ -37,6 +70,7 @@ public class Move {
     }
 
     private void chekList() {
+
         if (getCounterValues() == (int) numberStep.get(1)) {
             numberStep.set(0, getCounterValues());
         }
@@ -51,118 +85,130 @@ public class Move {
         }
 
         if (numberStep.get(0).equals(numberStep.get(1)) &&
-                numberStep.get(1).equals(numberStep.get(2)) && numberStep.get(2).equals(getCounterValues())) {
-            booleanWile = false;
-            System.out.println("минимально количество ходов = " + getCounterValues() * 5);
+                numberStep.get(1).equals(numberStep.get(2)) &&
+                numberStep.get(2).equals(getCounterValues())) {
+            if (getPartOneValue() == getCounterValues()) {
+                booleanWile = false;
+                System.out.println("минимально количество ходов = " + getCounterValues() * 5 + "секунд");
+            }
+            if (getCounterValues() < getPartOneValue()) {
+                booleanWile = false;
+                System.out.println("минимально количество ходов = " + getCounterValues() * 5 + "секунд");
+            }
+            if (getPartOneValue() < getCounterValues()) {
+                setPartOneValue(getCounterValues());
+            }
+            setCounterValues(0);
         }
-
-
     }
 
-    public void move() {
+    public void start() {
         booleanWile = true;
         numberStep.add(0, 1000);
         numberStep.add(1, 1001);
         numberStep.add(2, 1002);
         GenerateLocate generateLocate = new GenerateLocate(H_LEVEL, M_LINE, N_COLUMN);
         setGenerateLocate(generateLocate.generateLocate());
+        for (int i = 0; i < getGenerateLocate().size(); i++) {
+            char[][] arrayForMap = (char[][]) getGenerateLocate().get(i + 1);
+            printLocat(arrayForMap);
+            System.out.println("-----------");
+        }
+        setCounterValues(0);
+        heroes.setLine(0);
+        setNextValue('.');
+        heroes.setColumn(0);
+        setLevelLocate(1);
+        setCounter(0);
+
+    }
+
+
+    public void move() {
+        start();
+        int oldXValue = heroes.getLine();
+        int oldYValue = heroes.getColumn();
         while (booleanWile) {
-            setCounterValues(0);
-            heroes.setLine(0);
-            nextValue = '.';
-            heroes.setColumn(0);
-            levelLocate = 1;
-            int counter = 0;
-            for (int i = 0; nextValue != '2'; i++) {
-                if (i < 50) {
+            for (int i = 0; getNextValue() != '2'; i++) {
+                if (i < 100) {
                     int chance = random.nextInt(5) + 1;
                     if (chance == 1) {
-                        char[][] mapForChance = (char[][]) getGenerateLocate().get(levelLocate);
+                        char[][] mapForChance = (char[][]) getGenerateLocate().get(getLevelLocate());
                         if (heroes.getColumn() + 1 == mapForChance[heroes.getLine()].length ||
                                 +mapForChance[heroes.getLine()][heroes.getColumn() + 1] == '0') {
                         } else {
-                            int oldXValue = heroes.getLine();
-                            int oldYValue = heroes.getColumn();
                             heroes.setColumn(heroes.getColumn() + 1);
-                            nextValue = mapForChance[heroes.getLine()][heroes.getColumn()];
-                            if (nextValue != '2') {
+                            setNextValue(mapForChance[heroes.getLine()][heroes.getColumn()]);
+                            if (getNextValue() != '2') {
                                 replaceHeroesPosition(oldXValue, oldYValue);
                             }
-                            ++counter;
+                            setCounter(getCounter() + 1);
                         }
                     }
                     if (chance == 2) {
-                        char[][] mapForChance = (char[][]) getGenerateLocate().get(levelLocate);
+                        char[][] mapForChance = (char[][]) getGenerateLocate().get(getLevelLocate());
                         if (heroes.getColumn() - 1 < 0 ||
                                 +mapForChance[heroes.getLine()][heroes.getColumn() - 1] == '0') {
 
                         } else {
-                            int oldXValue = heroes.getLine();
-                            int oldYValue = heroes.getColumn();
                             heroes.setColumn(heroes.getColumn() - 1);
-                            nextValue = mapForChance[heroes.getLine()][heroes.getColumn()];
-                            if (nextValue != '2') {
+                            setNextValue(mapForChance[heroes.getLine()][heroes.getColumn()]);
+                            if (getNextValue() != '2') {
                                 replaceHeroesPosition(oldXValue, oldYValue);
                             }
-                            ++counter;
+                            setCounter(getCounter() + 1);
                         }
                     }
                     if (chance == 3) {
-                        char[][] mapForChance = (char[][]) getGenerateLocate().get(levelLocate);
+                        char[][] mapForChance = (char[][]) getGenerateLocate().get(getLevelLocate());
                         if (heroes.getLine() + 1 == mapForChance.length ||
                                 +mapForChance[heroes.getLine() + 1][heroes.getColumn()] == '0') {
 
                         } else {
-                            int oldXValue = heroes.getLine();
-                            int oldYValue = heroes.getColumn();
                             heroes.setLine(heroes.getLine() + 1);
-                            nextValue = mapForChance[heroes.getLine()][heroes.getColumn()];
-                            if (nextValue != '2') {
+                            setNextValue(mapForChance[heroes.getLine()][heroes.getColumn()]);
+                            if (getNextValue() != '2') {
                                 replaceHeroesPosition(oldXValue, oldYValue);
                             }
-                            ++counter;
+                            setCounter(getCounter() + 1);
                         }
                     }
                     if (chance == 4) {
-                        char[][] mapForChance = (char[][]) getGenerateLocate().get(levelLocate);
+                        char[][] mapForChance = (char[][]) getGenerateLocate().get(getLevelLocate());
                         if (heroes.getLine() - 1 < 0 ||
                                 +mapForChance[heroes.getLine() - 1][heroes.getColumn()] == '0') {
 
                         } else {
-                            int oldXValue = heroes.getLine();
-                            int oldYValue = heroes.getColumn();
                             heroes.setLine(heroes.getLine() - 1);
-                            nextValue = mapForChance[heroes.getLine()][heroes.getColumn()];
-                            if (nextValue != '2') {
+                            setNextValue(mapForChance[heroes.getLine()][heroes.getColumn()]);
+                            if (getNextValue() != '2') {
                                 replaceHeroesPosition(oldXValue, oldYValue);
                             }
-                            ++counter;
+                            setCounter(getCounter() + 1);
                         }
                     }
                     if (chance == 5) {
-                        if (levelLocate + 1 <= getGenerateLocate().size()) {
-                            char[][] mapForChance = (char[][]) getGenerateLocate().get(levelLocate + 1);
+                        if (getLevelLocate() + 1 <= getGenerateLocate().size()) {
+                            char[][] mapForChance = (char[][]) getGenerateLocate().get(getLevelLocate() + 1);
                             if (mapForChance[heroes.getLine()][heroes.getColumn()] == '0') {
                             }
                             if (mapForChance[heroes.getLine()][heroes.getColumn()] == '2') {
-                                nextValue = '2';
-                                ++counter;
+                                setNextValue('2');
+                                setCounter(getCounter() + 1);
                             } else {
-                                char[][] locats = (char[][]) getGenerateLocate().get(levelLocate);
+                                char[][] locats = (char[][]) getGenerateLocate().get(getLevelLocate());
                                 locats[heroes.getLine()][heroes.getColumn()] = '.';
-                                levelLocate += 1;
-                                ++counter;
+                                setLevelLocate(getLevelLocate() + 1);
+                                setCounter(getCounter() + 1);
                             }
                         }
 
 
                     }
                 } else {
-                    setGenerateLocate(generateLocate.generateLocate());
-                    heroes.setLine(0);
-                    heroes.setColumn(0);
-                    levelLocate = 1;
+                    start();
                     i = 0;
+
                 }
                 if (nextValue == '2') {
                     break;
@@ -170,18 +216,10 @@ public class Move {
 
             }
             setCounterValues(counter);
-            System.out.println(getCounterValues());
             chekList();
 
         }
-        System.out.println("минимально количество ходов = " + getCounterValues() * 5);
-        for (int i = 0; i < getGenerateLocate().size(); i++) {
-            char[][] arrayForMap = (char[][]) getGenerateLocate().get(i + 1);
-            printLocat(arrayForMap);
-            System.out.println("-----------");
-        }
     }
-
 
     public static void printLocat(char[][] array) {
 
